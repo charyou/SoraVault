085ccb840ed1125ee9bb1e6d3cce66ec641f247b
charyou <charyou@gmx.de>
2026-04-24 01:16:58 +0200

    fix: update copyright information and simplify header comments

--- src/core.js
+++ src/core.js
@@ -1,12 +1,8 @@
 /*! 
-  SoraVault Core Logic
-  (c) 2026 Sebastian Haas (charyou)
-
-  THIRD-PARTY NOTICE:
-  The watermark removal and proxy logic is based on code by Casey Jardin.
+  SoraVault 2.7
   
   MIT License
-  Copyright (c) 2026 Casey Jardin
+  Copyright © 2026 Sebastian Haas (charyou)
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +21,10 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
+
+  THIRD-PARTY NOTICE:
+  The watermark removal logic is based on code by Casey Jardin.
+
 */
 
 
